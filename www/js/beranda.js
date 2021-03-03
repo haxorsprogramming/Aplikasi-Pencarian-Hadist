7004f20e06c80102ffbf3894723d227faba2c3e9
haxorsprogramming <haxorsuinsu@gmail.com>
2021-03-03 15:19:23 +0700

    update

--- www/js/beranda.js
+++ www/js/beranda.js
@@ -1,39 +1,33 @@
-$(document).ready(function(){
-  $(".materialboxed").materialbox();
-  $(".button-collapse").sideNav();
-  $("select").material_select();
-  var loading = "<div class='progress'><div class='indeterminate'></div></div>";
-  console.log("Halaman berhasil di load");
-  $("#divUtama").html(loading);
-  $("#divUtama").load("utama.html");
-  $("#judulApps").html("Aplikasi Klasifikasi Penyakit Kulit");
-  //tombol beranda
-  $("#homeSideNav").click(function () {
-    location.reload();
-  });
-
-  function updateSistem() {
-    $('#divUtama').load('')
-    Materialize.toast("Tak ada respon dari server", 1100);
-  }
-
-});
-
 var divApps = new Vue({
-  el : '#divApps',
-  data : {
-    developer : 'Rini Fadillah',
+  el: "#divApps",
+  data: {
+    developer: "Rizka Junainah",
   },
-  methods : {
-    pengujianAtc : function()
-    {
-      $('#divUtama').load('pengujian.html');
-      $('.button-collapse').sideNav('hide');
+  methods: {
+    pengujianAtc: function () {
+      $("#divUtama").load("pengujian.html");
+      $(".button-collapse").sideNav("hide");
     },
-    daftarPenyakitAtc : function()
-    {
-      $('#divUtama').load('daftarPenyakit.html');
-      $('.button-collapse').sideNav('hide');
-    }
-  }
+    daftarPenyakitAtc: function () {
+      $("#divUtama").load("daftarPenyakit.html");
+      $(".button-collapse").sideNav("hide");
+    },
+  },
 });
+
+
+// Inisialisasi 
+$(".materialboxed").materialbox();
+$(".button-collapse").sideNav();
+$("select").material_select();
+var loading = "<div class='progress'><div class='indeterminate'></div></div>";
+$("#divUtama").html(loading);
+$("#divUtama").load("utama.html");
+$("#judulApps").html("Aplikasi Pencarian Hadist Shahih, Hasan, Dha'if, dan Maudhu'");
+
+function updateSistem() {
+  $("#divUtama").load("");
+  Materialize.toast("Tak ada respon dari server", 1100);
+}
+
+
